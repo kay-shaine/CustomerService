1919333d972cc5912df0a7fafda520b53d648612
Enigbokan Kehinde Johnson <66526458+kay-shaine@users.noreply.github.com>
2022-09-03 09:18:32 +0100

    Add project files.

---
+++ CustomerService/Repository/CustomerRepo.cs
@@ -0,0 +1,56 @@
+﻿using CustomerService.Data;
+using CustomerService.Model;
+
+namespace CustomerService.Repository
+{
+    public class CustomerRepo : ICustomerRepo
+    {
+        private readonly CustomerDbContext _customerDb;
+
+        public CustomerRepo(CustomerDbContext customerDb)
+        {
+            _customerDb = customerDb;
+        }
+
+        public void CreateCustomer(Customer customer)
+        {
+            if (customer == null)
+            {
+                throw new ArgumentNullException(nameof(customer));
+            }
+
+            _customerDb.Customers.Add(customer);
+        }
+
+
+        public void DeleteCustomerByName(string name)
+        {
+            _customerDb.RemoveRange(name);
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _customerDb.Customers.ToList();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _customerDb.Customers.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public Customer GetCustomerByName(string name)
+        {
+            return _customerDb.Customers.Find(name);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_customerDb.SaveChanges() >= 0);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            _customerDb.Update(customer);
+        }
+    }
+}
---
+++ CustomerService/Repository/ICustomerRepo.cs
@@ -0,0 +1,18 @@
+﻿using CustomerService.Model;
+
+namespace CustomerService.Repository
+{
+    public interface ICustomerRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Customer> GetAllCustomers();
+
+        Customer GetCustomerById(int id);
+
+        Customer GetCustomerByName(string name);
+        void CreateCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomerByName(string name);
+    }
+}
